eacad26bcffd54064ff1e9a5ca874089edd2249f
Armindo Cachada <acachada@armindos-mbp.lan>
2020-08-11 12:51:38 +0100

    added wait for certificates to be generated

--- webadmin/start.sh
+++ webadmin/start.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+# wait until certificates are generated
+while [ ! -f "/etc/openvpn/ca.crt" ]
+do
+     sleep 1;
+     echo "Waiting for OpenVPN certificates to be generated";
+done
+
 cd /var/www/html
 # Copy ta.key inside the client-conf directory
 for directory in "./client-conf/gnu-linux/" "./client-conf/osx-viscosity/" "./client-conf/windows/"; do
@@ -8,15 +15,14 @@ for directory in "./client-conf/gnu-linux/" "./client-conf/osx-viscosity/" "./cl
     cp "/etc/openvpn/"{ca.crt,ta.key} $directory;
     chown -R www-data:www-data $directory
     # Replace in the client configurations with the ip of the server and openvpn protocol
-    for file in $(find -name client.ovpn); do
-        sed -i "s/remote xxx\.xxx\.xxx\.xxx 443/remote $HOST_ADDR $HOST_SSL_PORT/" $file;
-        echo "<ca>" >> $file;
-        cat "/etc/openvpn/ca.crt" >> $file;
-        echo "</ca>" >> $file;
-        echo "<tls-auth>" >> $file;
-        cat "/etc/openvpn/ta.key" >> $file;
-        echo "</tls-auth>" >> $file;
-    done
+    file="$directory/client.ovpn";
+    sed -i "s/remote xxx\.xxx\.xxx\.xxx 443/remote $HOST_ADDR $HOST_SSL_PORT/" $file;
+    echo "<ca>" >> $file;
+    cat "/etc/openvpn/ca.crt" >> $file;
+    echo "</ca>" >> $file;
+    echo "<tls-auth>" >> $file;
+    cat "/etc/openvpn/ta.key" >> $file;
+    echo "</tls-auth>" >> $file;
   fi
 done
 
